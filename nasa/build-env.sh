ac30b60288130e878d673bf026666c72112ed397
grace.llewellyn <grace.llewellyn@jpl.nasa.gov>
2024-02-12 14:19:41 -0800

    update paths in NASA run.sh and build-env.sh

--- nasa/build-env.sh
+++ nasa/build-env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
+REPO_ROOT_PATH=$(dirname ${basedir})
+
 conda config --set solver libmamba
 
-conda env create -f ${basedir}/environment.yaml
+conda env create -f ${REPO_ROOT_PATH}/environment.yaml
 
